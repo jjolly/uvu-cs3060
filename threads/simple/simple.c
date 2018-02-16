aa079d0fcad277ed1d0efbce834e8125ffff3576
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-15 23:18:30 -0700

    Added basic threading code to thread-simple

--- threads/simple/simple.c
+++ threads/simple/simple.c
@@ -1,3 +1,31 @@
+#include<stdio.h>
+#include<pthread.h>
+
+/* Simple child thread function */
+void *child_thread_func(void *p) {
+  printf("Child thread created\n");
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
+  /* Thread handle */
+  pthread_t thread_handle;
+  int ret;
+
+  /* Create child thread */
+  ret = pthread_create(&thread_handle, NULL, child_thread_func, NULL);
+
+  if(ret != 0) {
+    fprintf(stderr, "Error creating child thread\n");
+    return -1;
+  }
+
+  ret = pthread_join(thread_handle, NULL);
+
+  if(ret != 0) {
+    fprintf(stderr, "Error joining child thread\n");
+    return -1;
+  }
+
   return 0;
 }
