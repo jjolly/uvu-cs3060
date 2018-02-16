c82162b2e847c3157a711c2a25e84bb0332629fc
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-15 23:27:14 -0700

    Added inc-dec threads to threads-inc-dec. It produces invalid results

--- threads/inc-dec/inc-dec.c
+++ threads/inc-dec/inc-dec.c
@@ -1,3 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+#define CHANGE_AMOUNT (1000000)
+
+/* Increment thread function */
+void *inc_thread_func(void *p) {
+  int *num = (int *)p;
+  int i;
+
+  for(i = 0; i < CHANGE_AMOUNT; i++) {
+    (*num)++;
+  }
+
+  return NULL;
+}
+
+/* Decrement thread function */
+void *dec_thread_func(void *p) {
+  int *num = (int *)p;
+  int i;
+
+  for(i = 0; i < CHANGE_AMOUNT; i++) {
+    (*num)--;
+  }
+
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
+  pthread_t ith, dth;
+  int ret, num = 0;
+
+  ret = pthread_create(&ith, NULL, inc_thread_func, &num);
+  if(ret != 0) {
+    fprintf(stderr, "Unable to create increment thread\n");
+    return -1;
+  }
+
+  ret = pthread_create(&dth, NULL, dec_thread_func, &num);
+  if(ret != 0) {
+    fprintf(stderr, "Unable to create decrement thread\n");
+    return -1;
+  }
+
+  ret = pthread_join(ith, NULL);
+  if(ret != 0) {
+    fprintf(stderr, "Unable to join increment thread\n");
+    return -1;
+  }
+
+  ret = pthread_join(dth, NULL);
+  if(ret != 0) {
+    fprintf(stderr, "Unable to join decrement thread\n");
+    return -1;
+  }
+
+  printf("Final result: %d\n", num);
+
   return 0;
 }
