c2e5083dacc95320c50f6092df5458486ff23168
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-16 06:59:21 -0700

    Changed threads interleave to create two threads that print a character

--- threads/interleave/interleave.c
+++ threads/interleave/interleave.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <pthread.h>
 
+struct char_print_params {
+  char print_char;
+  int number_to_print;
+};
+
 /* the thread function */
-void *printxs(void *unused) {
-  while(1)
-    fputc('x', stderr);
+void *char_print(void *params) {
+  struct char_print_params *p;
+  p = (struct char_print_params *) params;
+
+  int i;
+  for (i = 0; i < p->number_to_print; i++)
+    fputc(p->print_char, stderr);
 
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
   /* the thread handle */
-  pthread_t x_thread;
+  pthread_t thread1_handle;
+  pthread_t thread2_handle;
 
-  pthread_create(&x_thread, NULL, &printxs, NULL);
+  struct char_print_params thread1_args;
+  struct char_print_params thread2_args;
 
-  while(1)
-    fputc('o', stderr);
+  thread1_args.print_char = 'x';
+  thread1_args.number_to_print = 3000;
+  pthread_create(&thread1_handle, NULL, char_print, &thread1_args);
+
+  thread2_args.print_char = 'y';
+  thread2_args.number_to_print = 5000;
+  pthread_create(&thread2_handle, NULL, char_print, &thread2_args);
 
   return 0;
 }
