98d8db411216c48ccac4bfaf850352d5b9ca240c
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-16 06:54:03 -0700

    Changed threads interleave to output endless characters for comparison

--- threads/interleave/interleave.c
+++ threads/interleave/interleave.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+
+/* the thread function */
+void *printxs(void *unused) {
+  while(1)
+    fputc('x', stderr);
+
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
+  /* the thread handle */
+  pthread_t x_thread;
+
+  pthread_create(&x_thread, NULL, &printxs, NULL);
+
+  while(1)
+    fputc('o', stderr);
+
   return 0;
 }
