0c4a3400fedb874c13bb6e70378916cd1cf0f5bf
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-15 23:20:53 -0700

    Added to threads-simple value passing to and from the child thread

--- threads/simple/simple.c
+++ threads/simple/simple.c
@@ -1,31 +1,52 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 
 /* Simple child thread function */
 void *child_thread_func(void *p) {
+  /* Cast void pointer to an integer pointer */
+  int *number = (int *)p;
+
+  /* Allocate integer pointer on the heap */
+  int *result = malloc(sizeof(int));
+
+  /* Square number */
+  *result = *number * *number;
+
   printf("Child thread created\n");
-  return NULL;
+
+  /* Send result (in heap) back to the parent */
+  return result;
 }
 
 int main(int argc, char *argv[]) {
   /* Thread handle */
   pthread_t thread_handle;
+  void *result;
   int ret;
+  int number = 10;
 
   /* Create child thread */
-  ret = pthread_create(&thread_handle, NULL, child_thread_func, NULL);
+  /* Send pointer to local variable */
+  ret = pthread_create(&thread_handle, NULL, child_thread_func, &number);
 
   if(ret != 0) {
     fprintf(stderr, "Error creating child thread\n");
     return -1;
   }
 
-  ret = pthread_join(thread_handle, NULL);
+  /* Retrieve result by passing a reference to a void pointer */
+  ret = pthread_join(thread_handle, &result);
 
   if(ret != 0) {
     fprintf(stderr, "Error joining child thread\n");
     return -1;
   }
+
+  printf("Result of %d squared is %d\n", number, *(int *)result);
+
+  /* Free allocated memory */
+  free(result);
 
   return 0;
 }
