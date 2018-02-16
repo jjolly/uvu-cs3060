8a33a57793641d954240bc1eaa72b4cc031c646d
Mr. Jolly To You <jollyjo@uvu.edu>
2018-02-15 23:38:33 -0700

    threads inc-dec using pthreads mutex

--- threads/inc-dec/inc-dec.c
+++ threads/inc-dec/inc-dec.c
@@ -4,19 +4,24 @@
 
 #define CHANGE_AMOUNT (1000000)
 
-int lock_var = 0;
+pthread_mutex_t lock_var = PTHREAD_MUTEX_INITIALIZER;
 
-/* Mutual Exclusion Lock using compare-and-swap */
-void compare_and_swap_lock(int *lock) {
-  /* Perform CMPXCGH machine instruction on Intel CPUs */
-  /* This will only do the swap if the test is successful */
-  /* Returns False (0) if swap did not happen */
-  while(__sync_bool_compare_and_swap(lock, 0, 1) == 0);
+/* Mutual Exclusion Lock using pthread mutex */
+void mutex_lock(pthread_mutex_t *lock) {
+  int ret = pthread_mutex_lock(lock);
+  if(ret != 0) {
+    fprintf(stderr, "Failed to lock mutex\n");
+    exit(-1);
+  }
 }
 
-/* Mutual Exclusion Unlock for compare-and-swap */
-void compare_and_swap_unlock(int *lock) {
-  *lock = 0;
+/* Mutual Exclusion Unlock for pthread mutex */
+void mutex_unlock(pthread_mutex_t *lock) {
+  int ret = pthread_mutex_unlock(lock);
+  if(ret != 0) {
+    fprintf(stderr, "Failed to unlock mutex\n");
+    exit(-1);
+  }
 }
 
 /* Increment thread function */
@@ -25,9 +30,9 @@ void *inc_thread_func(void *p) {
   int i, tid = 0;
 
   for(i = 0; i < CHANGE_AMOUNT; i++) {
-    compare_and_swap_lock(&lock_var);
+    mutex_lock(&lock_var);
     (*num)++;
-    compare_and_swap_unlock(&lock_var);
+    mutex_unlock(&lock_var);
   }
 
   return NULL;
@@ -39,9 +44,9 @@ void *dec_thread_func(void *p) {
   int i, tid = 1;
 
   for(i = 0; i < CHANGE_AMOUNT; i++) {
-    compare_and_swap_lock(&lock_var);
+    mutex_lock(&lock_var);
     (*num)--;
-    compare_and_swap_unlock(&lock_var);
+    mutex_unlock(&lock_var);
   }
 
   return NULL;
