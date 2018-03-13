2fea350dd0ccae6b4ff8a5bffef555b8ddf9417f
Mr. Jolly To You <jollyjo@uvu.edu>
2018-03-12 22:16:19 -0600

    Simplified prod-cons semaphore example

--- threads/prod-cons/prod-cons.c
+++ threads/prod-cons/prod-cons.c
@@ -6,14 +6,10 @@
 #define COUNT_TO_PRODUCE (1000000)
 #define PROD_CONS_CHECK_VAL (-1)
 #define MAX_BUFFER_LEN (20)
-#define COUNT_OF_CONSUMERS (2)
 
-/* Lockless producer-consumer is possible, but only for */
-/* single producer/single consumer */
+/* Semaphores to help control buffer content */
 struct pc_buff {
-  int pi, ci;
   int buffer[MAX_BUFFER_LEN];
-  pthread_mutex_t mutex;
   sem_t items, empty;
 };
 
@@ -25,16 +21,16 @@ void pthread_ret_error(const char *msg) {
 /* Producer thread function */
 void *producer_thread_func(void *p) {
   struct pc_buff *pcb = (struct pc_buff *)p;
-  int i;
+  int i, pi = 0;
 
   for(i = 0; i < COUNT_TO_PRODUCE; i++) {
     /* Let the semaphore perform the spinlock for an empty space as necessary */
     sem_wait(&pcb->empty);
 
     /* Just put the number one in the buffer */
-    pcb->buffer[pcb->pi] = 1;
+    pcb->buffer[pi] = 1;
     /* Increment the producer buffer index by one circularly */
-    pcb->pi = (pcb->pi + 1) % MAX_BUFFER_LEN;
+    pi = (pi + 1) % MAX_BUFFER_LEN;
 
     /* Indicate that one more item has been added to the buffer */
     sem_post(&pcb->items);
@@ -42,9 +38,9 @@ void *producer_thread_func(void *p) {
   /* Done producing. Indicate this is finished */
   sem_wait(&pcb->empty);
   /* The check value -1 should never occur during production */
-  pcb->buffer[pcb->pi] = PROD_CONS_CHECK_VAL;
+  pcb->buffer[pi] = PROD_CONS_CHECK_VAL;
   /* Let the consumers know there's one last value to use */
-  pcb->pi = (pcb->pi + 1) % MAX_BUFFER_LEN;
+  pi = (pi + 1) % MAX_BUFFER_LEN;
   sem_post(&pcb->items);
 
   return NULL;
@@ -53,7 +49,7 @@ void *producer_thread_func(void *p) {
 /* Consumer thread function */
 void *consumer_thread_func(void *p) {
   struct pc_buff *pcb = (struct pc_buff *)p;
-  int val;
+  int val, ci = 0;
   int *result = malloc(sizeof(int));
   *result = 0;
 
@@ -62,21 +58,17 @@ void *consumer_thread_func(void *p) {
     /* Let the semaphore perform the spinlock for items as necessary */
     sem_wait(&pcb->items);
 
-    /* Use the mutex to protect the consumer index from a race condition */
-    pthread_mutex_lock(&pcb->mutex);
-    val = pcb->buffer[pcb->ci];
+    val = pcb->buffer[ci];
     /* Check value found, stop looping */
     if(PROD_CONS_CHECK_VAL == val) break;
     /* Increment the consumer buffer index by one circularly */
-    pcb->ci = (pcb->ci + 1) % MAX_BUFFER_LEN;
-    pthread_mutex_unlock(&pcb->mutex);
+    ci = (ci + 1) % MAX_BUFFER_LEN;
 
     /* Bump the empty semaphore to indicate there is one more free space to use */
     sem_post(&pcb->empty);
 
     *result += val;
   }
-  pthread_mutex_unlock(&pcb->mutex);
   /* Pretend that this thread never removed the item from the buffer */
   sem_post(&pcb->items);
 
@@ -84,13 +76,10 @@ void *consumer_thread_func(void *p) {
 }
 
 int main(int argc, char *argv[]) {
-  pthread_t pth, cth[COUNT_OF_CONSUMERS];
+  pthread_t pth, cth;
   int sum, ret, i;
   void *result;
   struct pc_buff *pcb = malloc(sizeof(struct pc_buff));
-  pcb->pi = 0;
-  pcb->ci = 0;
-  pthread_mutex_init(&pcb->mutex, NULL);
   /* Keep track of the number of items in the buffer */
   sem_init(&pcb->items, 0, 0);
   /* Keep track of the number of available spaces in the buffer */
@@ -99,21 +88,17 @@ int main(int argc, char *argv[]) {
   ret = pthread_create(&pth, NULL, producer_thread_func, pcb);
   if(ret != 0) pthread_ret_error("Unable to create producer thread");
 
-  for(i = 0; i < COUNT_OF_CONSUMERS; i++) {
-    ret = pthread_create(&cth[i], NULL, consumer_thread_func, pcb);
-    if(ret != 0) pthread_ret_error("Unable to create consumer thread");
-  }
+  ret = pthread_create(&cth, NULL, consumer_thread_func, pcb);
+  if(ret != 0) pthread_ret_error("Unable to create consumer thread");
 
   ret = pthread_join(pth, NULL);
   if(ret != 0) pthread_ret_error("Unable to join producer thread");
 
   sum = 0;
-  for(i = 0; i < COUNT_OF_CONSUMERS; i++) {
-    ret = pthread_join(cth[i], &result);
-    if(ret != 0) pthread_ret_error("Unable to join consumer thread");
+  ret = pthread_join(cth, &result);
+  if(ret != 0) pthread_ret_error("Unable to join consumer thread");
   
-    sum += *(int *)result;
-  }
+  sum += *(int *)result;
 
   printf("Sum result: %d\n", sum);
 
